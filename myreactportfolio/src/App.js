2ce5b44f805c2da9d9c704af249092be393cadd5
Bright <113262558+kagebright@users.noreply.github.com>
2023-02-26 19:39:37 -0500

    file restructure

--- myreactportfolio/src/App.js
+++ myreactportfolio/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
+import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/aboutme" component={AboutMe} />
+          <Route path="/Portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/Resume" component={Resume} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
