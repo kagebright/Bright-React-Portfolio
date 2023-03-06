e346c99c2cc5b7e7d0b21059be00d5513465ddab
Bright <113262558+kagebright@users.noreply.github.com>
2023-03-05 20:28:43 -0500

    ...

--- myreactportfolio/src/App.test.js
+++ myreactportfolio/src/App.test.js
@@ -1,39 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import pretty from 'pretty';
 import App from './App';
 
-test('renders header with developer name', () => {
-  render(<App />);
-  const headerElement = screen.getByRole('banner');
-  const developerNameElement = screen.getByText(/Developer Name/i);
-  expect(headerElement).toBeInTheDocument();
-  expect(developerNameElement).toBeInTheDocument();
+let container = null;
+
+beforeEach(() => {
+  // Setup a DOM element as the target
+  container = document.createElement('div');
+  document.body.appendChild(container);
 });
 
-test('renders navigation links', () => {
-  render(<App />);
-  const aboutLinkElement = screen.getByRole('link', { name: /About Me/i });
-  const portfolioLinkElement = screen.getByRole('link', { name: /Portfolio/i });
-  const contactLinkElement = screen.getByRole('link', { name: /Contact/i });
-  const resumeLinkElement = screen.getByRole('link', { name: /Resume/i });
-  expect(aboutLinkElement).toBeInTheDocument();
-  expect(portfolioLinkElement).toBeInTheDocument();
-  expect(contactLinkElement).toBeInTheDocument();
-  expect(resumeLinkElement).toBeInTheDocument();
+afterEach(() => {
+  // Cleanup on exiting to prevent this test from altering the results of future tests
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 });
-
-test('renders default About Me section', () => {
-  render(<App />);
-  const defaultSectionElement = screen.getByRole('region', { name: /About Me/i });
-  expect(defaultSectionElement).toBeInTheDocument();
-});
-
-test('switches to Portfolio section when Portfolio link is clicked', () => {
-  render(<App />);
-  const portfolioLinkElement = screen.getByRole('link', { name: /Portfolio/i });
-  const portfolioSectionElement = screen.getByRole('region', { name: /Portfolio/i });
-  expect(portfolioSectionElement).not.toBeVisible();
-  userEvent.click(portfolioLinkElement);
-  expect(portfolioSectionElement).toBeVisible();
-});
-
-// and so on for the remaining acceptance criteria
